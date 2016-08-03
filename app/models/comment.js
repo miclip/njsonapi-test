ae9ad38e705abf999ef9c359ec95da8aee2fa055
Michael Lipscombe <michael.lipscombe@aig.com>
2016-08-03 17:02:43 -0400

    Comments & People

--- app/models/comment.js
+++ app/models/comment.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
      body: DS.attr('string'),
-   author: DS.belongsTo('person') 
+     author: DS.belongsTo('person') 
 });
--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('articles');
-  this.route('test');
+  this.route('comments');
 });
 
 export default Router;
